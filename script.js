a1b338389843ef212bb7e71dc393d1e69cd89685
RawRaw5 <94robertwhite@gmail.com>
2022-02-19 23:21:46 -0800

    wordsearch update 5

--- script.js
+++ script.js
@@ -87,23 +87,3 @@ window.onload = function() {
         node.textContent = String.fromCharCode(charCode)
     }
 }
-
-    //  const startingMinutes = 10;
-    //     let time = startingMinutes * 60;
-
-    //     const countdownEl = document.getElementById('countdown');
-
-    //     setInterval(updateCountdown, 1000)
-
-    //     function updateCountdown() {
-    //     const minutes = math.floor(time / 60);
-    //     let seconds = time % 60;
-
-    //     seconds = seconds < 10 ? '0' + seconds : seconds;
-
-    //     countdownEl.innerHTML = '${minutes}: ${seconds}';
-    //     time--;
-       
-    //     };
-    
-
